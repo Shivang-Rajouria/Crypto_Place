edeb13a7f185305f2b44dbc2ed93f2087fbebc0b
Shivang Rajouria <shivangrajouria17@gmail.com>
2024-08-26 01:11:45 +0530

    First commit with Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import arrow_icon from '../../assets/arrow_icon.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={logo} alt="" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Featues</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className='nav-links'>
+      <select>
+        <option value="Eur">EUR</option>
+        <option value="USD">USD</option>
+        <option value="INR">INR</option>
+      </select>
+      <button >
+        Signup
+        <img src={arrow_icon} alt="" />
+      </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
